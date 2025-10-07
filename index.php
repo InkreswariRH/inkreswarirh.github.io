7c3d01c865d0a338b158304605cdc65e638df063
inkreswarirh <inkreswari@gmail.com>
2025-10-08 03:54:21 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,247 @@
+<?php include "header.html"; ?>
+
+        <!-- Start Clgun Banner 2 Area -->
+        <div class="banner-area-2 big-bg-2">
+            <div class="container">
+                <div class="banner-content-2">
+                    <div class="content">
+                        <span data-aos="fade-zoom-in" data-aos-delay="300">Faculty of Economis and Business</span><br>
+                        <span data-aos="fade-zoom-in" data-aos-delay="300">Jakarta State University</span>
+                        <h1 data-aos="fade-up" data-aos-delay="200">Digital Business</h1>
+                        <p data-aos="fade-up" data-aos-delay="300">This study program aims to develop students' abilities to understand and apply digital technology in the field of business. Students will learn how to analyze, design, and implement innovative digital business solutions, taking into account strategic and operational aspects.</p>
+                        <!-- <div class="buttons-action" data-aos="fade-up" data-aos-delay="100">
+                            <a class="default-btn" href="application-form.html">Apply to USD</a>
+                            <a class="default-btn btn-style-2" href="contact-us.html">Chat with Us</a>
+                        </div> -->
+                        <div class="vertical-lr" data-aos="fade-zoom-in" data-aos-delay="100">
+                            <p>Digital Business</p>
+                        </div>
+
+                        <div class="scroll-down" data-aos="fade-down" data-aos-delay="100">
+                           <a href="#about"><i class='bx bx-chevron-down'></i></a> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Clgun Banner 2 Area -->
+
+        <!-- Start About Us Area 2 -->
+        <div id="about" class="about-us-area-2 ptb-100">
+            <div class="container">
+                <div class="section-title" data-aos="fade-up" data-aos-delay="100">
+                    <h2>Meet Our Lecturers</h2>
+                    <p>
+                        Our study program is proud to be supported by a team of dedicated, experienced, and passionate lecturers who bring academic excellence and real-world insights into the classroom. Each of our lecturers plays a vital role in shaping future professionals through their teaching, research, and mentorship.
+                    </p>
+                </div>
+                <div class="about-content-courser owl-carousel owl-theme">
+                    <div class="content-items" data-dot="<button>01</button>">
+                        <div class="image ct-bg-1" data-aos="fade-zoom-in" data-aos-delay="100">
+                        </div>
+                    </div>
+                    <div class="content-items" data-dot="<button>02</button>">
+                        <div class="image ct-bg-2" data-aos="fade-zoom-in" data-aos-delay="100">
+                        </div>
+                    </div>
+                    <div class="content-items" data-dot="<button>03</button>">
+                        <div class="image ct-bg-3" data-aos="fade-zoom-in" data-aos-delay="100">
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <!-- End About Us Area 2 -->
+
+        <!-- Start Features Area 2 -->
+        <div class="features-area-2">
+            <div class="features-content-2 ptb-100">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+                            <div class="sub-title">
+                                <p>Showcase of Our Works</p>
+                                <div class="arrow-icon">
+                                        <img src="assets/img/icon/shape-1.png" alt="image">
+                                    </div>
+                            </div>
+
+                            <div class="content">
+                                <h2>A collection of activities and media content by the Digital Business Study Program</h2>
+                                
+                            <div class="row">
+                                <div class="item">
+                                    <div class="col-lg-6">
+                                        <div class="academic-item" data-aos="fade-up" data-aos-delay="300">
+                                            <div class="image">
+                                                <img src="https://img.youtube.com/vi/-IsMvqt8LWw/hqdefault.jpg" alt="IISMA VMU Thumbnail">
+                                            </div>
+                                            <div class="content">
+                                                <h4>IISMA VMU Mahasiswa Prodi Bisnis Digital UNJ 2024</h4>
+                                                <a class="btn" href="https://youtu.be/-IsMvqt8LWw" target="blank">Learn More <i class='bx bx-right-arrow-alt'></i></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>  
+                        </div>
+
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="academic-content">
+                                <div class="row">
+                                    <div class="col-lg-6 col-sm-6 col-md-6 pt-25">
+                                        <div class="academic-item" data-aos="fade-up" data-aos-delay="100">
+                                            <div class="image">
+                                                <img src="https://img.youtube.com/vi/gAyknUVrJVM/hqdefault.jpg" alt="image">
+                                            </div>
+                                            <div class="content">
+                                                <h4>Talk The Manager #1 | Rahasia Sukses Magang, PKL dan Skripsi</h4>
+                                                <a class="btn" href="https://youtu.be/gAyknUVrJVM" target="blank">Learn More <i class='bx bx-right-arrow-alt'></i></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-sm-6 col-md-6">
+                                        <div class="academic-item" data-aos="fade-up" data-aos-delay="200">
+                                            <div class="image">
+                                                <img src="assets/img/all-img/jodigbi.jpg" alt="image" style="width: 350px;height: 280px;">
+                                            </div>
+                                            <div class="content">
+                                                <h4>Journal of Digital Business and Global Economy</h4>
+                                                <a class="btn" href="https://journal.atdbi.or.id/" target="blank">Learn More <i class='bx bx-right-arrow-alt'></i></a>
+                                            </div>
+                                            
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Features Area 2 -->
+
+        <!-- Start News Area 2 -->
+        <div class="news-area ptb-100">
+            <div class="container">
+                <div class="section-title section-title-2" data-aos="fade-up" data-aos-delay="100">
+                    <div class="sub-title">
+                        <p>Our Video Profile</p>
+                    </div>
+                    <h2 style="text-align: center;">Watch our video to explore the spirit, journey, and innovation of Digital Business at UNJ</h2>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="video-area">
+                        <div class="container">
+                            <div class="video-play-btn" data-aos="fade-zoom-in" data-aos-delay="100">
+                                <a href="https://youtu.be/0vKLi0eDDIY" target="_blank">Play</a>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+
+                <div class="section-btn text-center" data-aos="fade-zoom-in" data-aos-delay="100">
+                    <p>Where Dreams Take Flight.</p>
+                </div>
+            </div>
+        </div>
+        <!-- End News Area 2 -->
+
+        
+
+        
+
+        <!-- Start Campus Area 2 -->
+        <div class="campus-area-2 ptb-100">
+            <div class="container-fluaid">
+                <div class="section-title section-title-2" data-aos="fade-up" data-aos-delay="100">
+                    <div class="sub-title">
+                        <p>The Campus Experience</p>
+                    </div>
+                    <h2>One University, Many Moments</h2>
+                </div>
+
+                <div class="campus-slider owl-carousel owl-theme">
+                    <div class="campus-card" data-aos="fade-up" data-aos-delay="20">
+                        <img src="assets/img/all-img/unj1.jpg" alt="image" style="width: 600px; height: auto;">
+                    </div>
+                    <div class="campus-card" data-aos="fade-up" data-aos-delay="20">
+                        <img src="assets/img/all-img/unj2.jpeg" alt="image" style="width: 600px; height: 338px;">
+                    </div>
+                    <div class="campus-card" data-aos="fade-up" data-aos-delay="20">
+                        <img src="assets/img/all-img/unj3.jpeg" alt="image" style="width: 600px; height: 338px;">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Campus Area -->
+
+        <!-- Start Success Area 2 -->
+        <div class="success-area success-area-2 ptb-100">
+            <div class="container">
+                <div class="section-title section-title-2" data-aos="fade-up" data-aos-delay="100">
+                    <div class="sub-title">
+                        <h2>Study Program Accreditation</h2>
+                    </div>
+                    <p>The Digital Business Study Program has earned the <b>Excellent</b> Accreditation, affirming the high standards of our curriculum, faculty, and student achievements.</p>
+                </div>
+    
+                <div class="row justify-content-center">
+                    <div class="col-lg-4 col-sm-6 col-md-6">
+                        <div class="success-card" data-aos="fade-up" data-aos-delay="100">
+                            <div class="image">
+                                <img src="assets/img/all-img/akre1.jpeg" alt="image" style="width: 424px; height: 320px;">
+                            </div>
+                            <ul>
+                                <li><a href="https://unj.ac.id/jadi-pelopor-dalam-pendidikan-bisnis-digital-di-indonesia-prodi-s1-bisnis-digital-feb-unj-raih-akreditasi-unggul-dari-lamemba/" target="blank"><h3>Our Accreditation</h3></a></li>
+                                <li class="link"><a href="https://unj.ac.id/jadi-pelopor-dalam-pendidikan-bisnis-digital-di-indonesia-prodi-s1-bisnis-digital-feb-unj-raih-akreditasi-unggul-dari-lamemba/" target="blank"><i class='bx bx-link-external'></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-6 col-md-6">
+                        <div class="success-card" data-aos="fade-up" data-aos-delay="200">
+                            <div class="image">
+                                <img src="assets/img/all-img/akre2.webp" alt="image" style="width: 424px; height: 320px;">
+                            </div>
+                                <ul>
+                                    <li><a href="https://edukasi.sindonews.com/read/1593851/211/prodi-s1-bisnis-digital-unj-jadi-yang-pertama-raih-akreditasi-unggul-lamemba-1752718054"><h3>Our Accreditation</h3></a></li>
+                                    <li class="link"><a href="https://edukasi.sindonews.com/read/1593851/211/prodi-s1-bisnis-digital-unj-jadi-yang-pertama-raih-akreditasi-unggul-lamemba-1752718054"><i class='bx bx-link-external'></i></a></li>
+                                </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-6 col-md-6">
+                        <div class="success-card" data-aos="fade-up" data-aos-delay="300">
+                            <div class="image">
+                                <img src="assets/img/all-img/akre3.jpg" alt="image" style="width: 424px; height: 320px;">
+                            </div>
+                            <ul>
+                                <li><a href="https://www.banpt.or.id/direktori/prodi/pencarian_prodi.php"></a></li>
+                                <li class="link"><a href="https://www.banpt.or.id/direktori/prodi/pencarian_prodi.php"><i class='bx bx-link-external' style="color:black;"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Success Area 2 -->
+
+        <!-- Start Subscribe Area 2 -->
+        <div class="subscribe-area subscribe-area-2">
+            <div class="container">
+                <div class="section-title section-title-2" data-aos="fade-up" data-aos-delay="100">
+                    <div class="sub-title">
+                        <p>Welcome to</p>
+                    </div>
+                    <h2>Digital Business <br>Study Program</br></h2>
+                </div>
+            </div>
+        </div> 
+        <!-- End Subscribe Area 2 -->
+
+        <?php include "footer.html"; ?>
+
